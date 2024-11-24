6b7c962b38bb67a03c0c7f949ef17b89f42b358c
Sid Senthil <sidsenthil2010@gmail.com>
2024-11-23 23:30:21 -0800

    Start working on the map

--- baneful-duality/player/player.gd
+++ baneful-duality/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var player_animations: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
+const SPEED = 275.0
 const JUMP_VELOCITY = -400.0
 var double_jump_count = 0
 
@@ -16,10 +16,10 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = 0.85 * JUMP_VELOCITY
+		velocity.y = 1 * JUMP_VELOCITY
 	if Input.is_action_just_pressed("move_up") and not is_on_floor() and double_jump_count == 0:
 		double_jump_count += 1
-		velocity.y = 0.85 * JUMP_VELOCITY
+		velocity.y = 1 * JUMP_VELOCITY
 				
 			
 	# Get the input direction and handle the movement/deceleration.
