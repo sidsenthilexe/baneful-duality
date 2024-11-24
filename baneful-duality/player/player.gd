01dfb7f673dcdb9c7626bbf84834148428425ee7
ItsGetWreckt <samanyu.rayala@gmail.com>
2024-11-23 20:25:17 -0800

    Fix double jump so that you can't use it infinitely

--- baneful-duality/player/player.gd
+++ baneful-duality/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player_animations: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -14,8 +14,9 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_up") and is_on_floor():
 		velocity.y = 0.7 * JUMP_VELOCITY
 		
-	if (Input.is_action_pressed("double_jump")) and not is_on_floor():
-		velocity.y = 0.5 * JUMP_VELOCITY
+	if (Input.is_action_just_pressed("double_jump")) and is_on_floor():
+		velocity.y = 0.7 * JUMP_VELOCITY
+		$double_jump_timer.start()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,11 +24,16 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	if (direction == 1):
-		anim.play("running")
+		player_animations.play("running")
 	elif (direction == -1):
-			anim.play("runningleft")
+		player_animations.play("runningleft")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("idle")
+		player_animations.play("idle")
 
 	move_and_slide()
+
+
+func _on_double_jump_timer_timeout():
+	velocity.y = 0.5 * JUMP_VELOCITY
+	$double_jump_timer.stop()
