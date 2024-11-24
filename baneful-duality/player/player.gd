bbde87500385eca0d4ae5bfb6547b7305360d78a
ItsGetWreckt <samanyu.rayala@gmail.com>
2024-11-23 21:06:14 -0800

    Fix double jump (good now)

--- baneful-duality/player/player.gd
+++ baneful-duality/player/player.gd
@@ -4,20 +4,24 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var double_jump_count = 0
 
 func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		double_jump_count = 0
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("move_up") and is_on_floor():
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
 		velocity.y = 0.7 * JUMP_VELOCITY
-		
-	if (Input.is_action_just_pressed("double_jump")) and is_on_floor():
+	if Input.is_action_just_pressed("move_up") and not is_on_floor() and double_jump_count == 0:
+		double_jump_count += 1
 		velocity.y = 0.7 * JUMP_VELOCITY
-		$double_jump_timer.start()
-
+				
+			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -32,8 +36,3 @@ func _physics_process(delta: float) -> void:
 		player_animations.play("idle")
 
 	move_and_slide()
-
-
-func _on_double_jump_timer_timeout():
-	velocity.y = 0.5 * JUMP_VELOCITY
-	$double_jump_timer.stop()
